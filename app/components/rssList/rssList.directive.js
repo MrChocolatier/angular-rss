92183f759f6eead2a62d389bf0c34e9933829911
Zar Zakharov <severenit@gmail.com>
2015-10-14 14:34:44 +0300

    Delete some line in directive

--- app/components/rssList/rssList.directive.js
+++ app/components/rssList/rssList.directive.js
@@ -6,55 +6,13 @@
     .directive('rssList', rssListDirective);
 
     //@ngInject
-    function rssListDirective(feedManage, manageRssList, dataShare) {
+    function rssListDirective(feedManage) {
       return {
         templateUrl: 'components/rssList/index.html',
         link: function(scope, element, attrs) {
           feedManage.getFeeds('feed').then(function(result) {
             scope.rssList = result;
           });
-
-          scope.entryChange = function(_boolean, _id) {
-
-          };
-            //scope.rssList = manageRssList.getRssList();
-            //scope.dataShare = dataShare;
-            //dataShare.feed = manageRssList.selectedRss();
-            //
-            //scope.add = function() {
-            //  scope.rssList = manageRssList.addRss({
-            //    name: scope.name,
-            //    url: scope.url
-            //  });
-            //
-            //  scope.name = '';
-            //  scope.url = '';
-            //}
-            //
-            //scope.delete = function(id, url) {
-            //  scope.rssList = manageRssList.deleteRss(id, url);
-            //}
-            //
-          scope.edit = function(_obj) {
-            console.log(_obj);
-          };
-            //
-            //scope.select = function(feed) {
-            //  manageRssList.selectedRss(feed);
-            //  dataShare.feed = feed;
-            //}
-            //
-            //function getRssObjIndex(list, url) {
-            //  var index;
-            //  list.some(function(entry, i) {
-            //    if (entry.url === url) {
-            //        index = i;
-            //        return true;
-            //    }
-            //  });
-            //
-            //  return index;
-            //}
         }
       }
     }
